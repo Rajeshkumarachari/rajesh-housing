a68f92bd621e11b278aebecfd63570fb9a5bcad8
Rajeshkumarachari <131753283+Rajeshkumarachari@users.noreply.github.com>
2024-11-10 22:15:45 +0530

    complete create listing page UI

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,44 +1,132 @@
 export default function CreateListing() {
   return (
-    <main className="flex  justify-center  mt-1 h-screen">
-      <div className="card bg-base-100 sm:w-[80%] w-[70%] h-fit shadow-xl">
-        <h1 className=" sm:text-3xl text-xl font-semibold text-center my-5">
-          CreateListing
-        </h1>
-        <form className="flex flex-col sm:flex:row">
-          <div className=" flex flex-col  gap-4 flex-1">
-            <label className="input input-bordered flex items-center gap-2 ">
-              Name :
-              <input
-                type="text"
-                className="grow"
-                id="name"
-                maxLength={62}
-                minLength={10}
-                required
-              />
-            </label>
-            <textarea
-              className="textarea textarea-secondary"
-              placeholder="Description"
-              id="description"
+    <main className="p-3 max-w-4xl mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">
+        Create a Listing
+      </h1>
+      <form className="flex flex-col sm:flex-row gap-4">
+        <div className="flex flex-col gap-4 flex-1">
+          <label className="input input-bordered flex items-center gap-2 border border-red-400">
+            Name
+            <input
+              type="text"
+              id="name"
+              maxLength="62"
+              minLength="10"
               required
-            ></textarea>
-            <label className="input input-bordered flex items-center gap-2 ">
-              Address :
-              <input
-                type="text"
-                className="grow"
-                id="address"
-                maxLength={62}
-                minLength={10}
-                required
-              />
-            </label>
-            <div className=""></div>
+              className="grow"
+            />
+          </label>
+          <textarea
+            type="text"
+            placeholder="Description"
+            className="textarea textarea-error"
+            id="description"
+            required
+          ></textarea>
+          <label className="input input-bordered flex items-center gap-2 border border-red-400">
+            Address
+            <input type="text" className="grow" id="address" required />
+          </label>
+          <div className="flex gap-6 flex-wrap">
+            <div className="flex gap-2">
+              <input type="checkbox" id="sale" className="w-5" />
+              <span>Sell</span>
+            </div>
+            <div className="flex gap-2">
+              <input type="checkbox" id="rent" className="w-5" />
+              <span>Rent</span>
+            </div>
+            <div className="flex gap-2">
+              <input type="checkbox" id="parking" className="w-5" />
+              <span>Parking spot</span>
+            </div>
+            <div className="flex gap-2">
+              <input type="checkbox" id="furnished" className="w-5" />
+              <span>Furnished</span>
+            </div>
+            <div className="flex gap-2">
+              <input type="checkbox" id="offer" className="w-5" />
+              <span>Offer</span>
+            </div>
           </div>
-        </form>
-      </div>
+          <div className="flex flex-wrap gap-6">
+            <div className="flex items-center gap-2">
+              <input
+                type="number"
+                id="bedrooms"
+                min="1"
+                max="10"
+                required
+                className="p-3 border border-gray-300 rounded-lg"
+              />
+              <p>Beds</p>
+            </div>
+            <div className="flex items-center gap-2">
+              <input
+                type="number"
+                id="bathrooms"
+                min="1"
+                max="10"
+                required
+                className="p-3 border border-gray-300 rounded-lg"
+              />
+              <p>Baths</p>
+            </div>
+            <div className="flex items-center gap-2">
+              <input
+                type="number"
+                id="regularPrice"
+                min="1"
+                max="10"
+                required
+                className="p-3 border border-gray-300 rounded-lg"
+              />
+              <div className="flex flex-col items-center">
+                <p>Regular price</p>
+                <span className="text-xs">($ / month)</span>
+              </div>
+            </div>
+            <div className="flex items-center gap-2">
+              <input
+                type="number"
+                id="discountPrice"
+                min="1"
+                max="10"
+                required
+                className="p-3 border border-gray-300 rounded-lg"
+              />
+              <div className="flex flex-col items-center">
+                <p>Discounted price</p>
+                <span className="text-xs">($ / month)</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col flex-1 gap-4">
+          <p className="font-semibold">
+            Images:
+            <span className="font-normal text-gray-600 ml-2">
+              The first image will be the cover (max 6)
+            </span>
+          </p>
+          <div className="flex gap-4">
+            <input
+              className="file-input file-input-bordered file-input-error w-full max-w-xs"
+              type="file"
+              id="images"
+              accept="image/*"
+              multiple
+            />
+            <button className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80">
+              Upload
+            </button>
+          </div>
+          <button className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+            Create Listing
+          </button>
+        </div>
+      </form>
     </main>
   );
 }
