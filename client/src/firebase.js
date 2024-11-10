4d18d912ba93413f087c5a6d4cb4eb44de7a1f12
Rajeshkumarachari <131753283+Rajeshkumarachari@users.noreply.github.com>
2024-11-10 13:46:17 +0530

    complete image upload functionality

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,3 +1,21 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: "rajesh-house.firebaseapp.com",
+//   projectId: "rajesh-house",
+//   storageBucket: "rajesh-house.firebasestorage.app",
+//   messagingSenderId: "561368534730",
+//   appId: "1:561368534730:web:afc3c56443ff8f6686ae33",
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -6,11 +24,11 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "rajesh-house.firebaseapp.com",
-  projectId: "rajesh-house",
-  storageBucket: "rajesh-house.firebasestorage.app",
-  messagingSenderId: "561368534730",
-  appId: "1:561368534730:web:afc3c56443ff8f6686ae33",
+  authDomain: "rajeshbnb-2c07c.firebaseapp.com",
+  projectId: "rajeshbnb-2c07c",
+  storageBucket: "rajeshbnb-2c07c.appspot.com",
+  messagingSenderId: "478923943755",
+  appId: "1:478923943755:web:33e0dba197bcd000aa02ce",
 };
 
 // Initialize Firebase
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,20 +1,90 @@
 import { useSelector } from "react-redux";
 import { FiDelete } from "react-icons/fi";
 import { IoIosLogOut } from "react-icons/io";
+import { useEffect, useRef, useState } from "react";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
 
 export default function Profile() {
   const { currentUser } = useSelector((state) => state.user);
+  const fileRef = useRef(null);
+  const [file, setFile] = useState(undefined);
+  const [filePerc, setFilePerc] = useState(0);
+  const [fileUploadError, setFileUploadError] = useState(false);
+  const [formData, setFormData] = useState({});
+
+  console.log(formData);
+  useEffect(() => {
+    if (file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+  const handleFileUpload = (file) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress));
+      },
+      (error) => {
+        setFileUploadError(true);
+        console.log(error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+          setFormData({ ...formData, avatar: downloadURL })
+        );
+      }
+    );
+    // setFilePerc(0);
+  };
+
   return (
     <div className="h-screen flex justify-center mt-3 ">
       <div className="card bg-base-100 sm:w-[40%] w-[80%] h-fit pb-5 shadow-xl px-3">
         <h1 className=" text-3xl font-medium text-center my-3">Profile</h1>
         <form className=" flex flex-col mx-20 ">
-          <div className="avatar flex justify-center">
+          <input
+            onChange={(e) => setFile(e.target.files[0])}
+            type="file"
+            hidden
+            ref={fileRef}
+            accept="image/*"
+          />
+          <div
+            onClick={() => fileRef.current.click()}
+            className="avatar flex justify-center"
+          >
             <div className=" sm:h-20 h-14  rounded-full cursor-pointer">
               <img
-                src={currentUser.avatar}
+                src={formData.avatar || currentUser.avatar}
                 className=" object-cover self-center"
               />
+              <p className="text-sm self-center">
+                {fileUploadError ? (
+                  <span className="text-red-700">
+                    Error Image upload (image must be less than 2 mb)
+                  </span>
+                ) : filePerc > 0 && filePerc < 100 ? (
+                  <span className="text-slate-700">{`Uploading ${filePerc}%`}</span>
+                ) : filePerc === 100 ? (
+                  <span className="text-green-700">
+                    Image successfully uploaded!
+                  </span>
+                ) : (
+                  ""
+                )}
+              </p>
             </div>
           </div>
           <label className="input input-bordered flex items-center gap-2 m-2">
@@ -33,7 +103,7 @@ export default function Profile() {
             Update
           </button>
         </form>
-        <div className=" flex justify-between mt-2 mx-10">
+        <div className=" flex justify-between mt-4 mx-10">
           <span className=" border-red-500 border font-medium text-red-700 cursor-pointer flex p-2 gap-1 items-center justify-center hover:bg-red-50 rounded-lg">
             Delete Account <FiDelete className=" size-5 " />
           </span>
